31bb83582aedc2b66e2b07d6bda5e40b1d15a522
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-08-18 23:29:13 -0700

    models created with any define

---
+++ models/index.js
@@ -0,0 +1,6 @@
+const Users = require('./users');
+const friends = require('./friends');
+const thoughts = require('./thoughts');
+const reactions = require('./reactions');
+
+module.exports = { Users, friends, thoughts, reactions };
