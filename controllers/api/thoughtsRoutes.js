0506fe9956902c9c9714299e86802de7ba89bd92
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-08-20 00:29:01 -0700

    all thoughts routes created and tested

--- controllers/api/thoughtsRoutes.js
+++ controllers/api/thoughtsRoutes.js
@@ -1,7 +1,53 @@
 const express = require('express');
 const router = express.Router();
-const thoughts  = require('../../models/thoughts');
+const { thoughts }  = require('../../models');
 
+router.get('/', async (req, res) => {
+    try {
+        const allThoughts = await thoughts.find();
+        res.status(200).json(allThoughts);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const thoughtData = await thoughts.findById(req.params.id);
+        res.status(200).json(thoughtData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const { thoughtText, username } = req.body;
+        const thoughtData = await thoughts.create({ thoughtText, username });
+        res.status(201).location(`/api/thoughts/${thoughtData._id}`).json(thoughtData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        const { thoughtText, username } = req.body;
+        const thoughtData = await thoughts.findByIdAndUpdate(req.params.id, { thoughtText, username });
+        res.status(200).json(thoughtData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const thoughtData = await thoughts.findByIdAndDelete(req.params.id);
+        res.status(200).json(thoughtData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 
 module.exports = router;
