34c242da6faeb5caf8aab9bc6d7adde21b5053dc
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-08-19 22:46:38 -0700

    get and post routes for users implemented and tested

---
+++ controllers/api/usersRoutes.js
@@ -0,0 +1,35 @@
+const { Users } = require('../../models');
+const { friends } = require('../../models');
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/', async (req, res) => {
+    try {
+        const allUsers = await Users.find();
+        res.status(200).json(allUsers);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const userData = await Users.findById(req.params.id).populate('friends').populate('thoughts');
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const { username, email } = req.body;
+        const userData = await Users.create({ username, email });
+        res.status(201).location(`/api/users/${userData._id}`).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
