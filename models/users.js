e95455f71ad24ec2a7ae9eeee21ffdd19ca75102
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-08-19 22:58:40 -0700

    seeded users! commented out to eliminate duplicate users

--- models/users.js
+++ models/users.js
@@ -7,14 +7,14 @@ const userSchema = new mongoose.Schema({
   friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'users' }],
 });
 
-const User = mongoose.model('users', userSchema);
+const Users = mongoose.model('users', userSchema);
 
-User.create([
-  { username: 'solomon22', email: 'solomon22@example.com', thoughts: [], friends: [], },
-  { username: 'jacob2', email: 'jacob2@example.com', thoughts: [], friends: [], },
-  { username: 'joseph2', email: 'josep2@test.com', thoughts: [], friends: [], },
-])
-  .then(users => console.log('Created users:', users))
-  .catch(err => console.log('Error creating users:', err));
+// User.create([
+//   { username: 'solomon22', email: 'solomon22@example.com', thoughts: [], friends: [], },
+//   { username: 'jacob2', email: 'jacob2@example.com', thoughts: [], friends: [], },
+//   { username: 'joseph2', email: 'josep2@test.com', thoughts: [], friends: [], },
+// ])
+//   .then(users => console.log('Created users:', users))
+//   .catch(err => console.log('Error creating users:', err));
 
-module.exports = User;
+module.exports = Users;
