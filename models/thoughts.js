e18f8eac022ead381245e25fbe693ac0795f29df
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-08-20 09:49:11 -0700

    adding virtual reaction count

--- models/thoughts.js
+++ models/thoughts.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 
+const reactionsSchema = new mongoose.Schema({
+  reactionBody: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  username: { type: String, required: true },
+});
+
 const thoughtSchema = new mongoose.Schema({
   thoughtText: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
@@ -7,23 +13,10 @@ const thoughtSchema = new mongoose.Schema({
   reactions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'reactions' }],
 });
 
-const thoughts = mongoose.model('thoughts', thoughtSchema);
-
-const reactionsSchema = new mongoose.Schema({
-  reactionBody: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
-  username: { type: String, required: true },
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
 });
 
-// thoughts 
-//   .create([
-//     { thoughtText: 'I love the way the ocean smells', username: 'solomon22', reactions: [] },
-//     { thoughtText: 'I love the way she observe people', username: 'jacob2', reactions: [] },
-//     { thoughtText: 'I love the way she is so smart', username: 'joseph2', reactions: [] },
-//   ])
-//   .then(thoughts => console.log('Created thoughts:', thoughts))
-//   .catch(err => console.log('Error creating thoughts:', err));
-
-
+const thoughts = mongoose.model('thoughts', thoughtSchema);
 
 module.exports = thoughts;
