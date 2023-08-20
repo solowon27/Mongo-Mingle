73f4280ccddf99c654b058436b865f99ab13e288
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-08-20 10:37:06 -0700

    using reactonSchema as reference reaction model

--- models/thoughts.js
+++ models/thoughts.js
@@ -10,7 +10,7 @@ const thoughtSchema = new mongoose.Schema({
   thoughtText: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
   username: { type: String, required: true },
-  reactions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'reactions' }],
+  reactions: [reactionsSchema],
 });
 
 thoughtSchema.virtual('reactionCount').get(function () {
