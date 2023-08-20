8642280fafc016122a264c5a1113635bc9819700
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-08-20 09:31:53 -0700

    Add thought object id to user's `thoughts` array

--- controllers/api/thoughtsRoutes.js
+++ controllers/api/thoughtsRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { thoughts }  = require('../../models');
+const { Users } = require('../../models');
 
 router.get('/', async (req, res) => {
     try {
@@ -22,13 +23,24 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
     try {
-        const { thoughtText, username } = req.body;
-        const thoughtData = await thoughts.create({ thoughtText, username });
-        res.status(201).location(`/api/thoughts/${thoughtData._id}`).json(thoughtData);
+      const { thoughtText, username } = req.body;
+  
+      const user = await Users.findOne({ username });
+      if (!user) {
+        return res.status(400).json({ message: 'User not found!' });
+      }
+  
+      const thoughtData = await thoughts.create({ thoughtText, username });
+      
+  //Add thought object id to user's `thoughts` array field
+      user.thoughts.push(thoughtData._id);
+      await user.save();
+  
+      res.status(201).location(`/api/thoughts/${thoughtData._id}`).json(thoughtData);
     } catch (err) {
-        res.status(500).json(err);
+      res.status(500).json(err);
     }
-});
+  });
 
 router.put('/:id', async (req, res) => {
     try {
