394ef6c6c3ac44ccd305eaa1ace44996114c7918
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-08-19 22:48:23 -0700

    models exported

--- models/index.js
+++ models/index.js
@@ -1,6 +1,5 @@
-const Users = require('./users');
+const Users = require('./Users');
 const friends = require('./friends');
 const thoughts = require('./thoughts');
-const reactions = require('./reactions');
 
-module.exports = { Users, friends, thoughts, reactions };
+module.exports = { Users, friends, thoughts };
