7c7918d658505eb8c3edb97d9cb4b0391ec7375b
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-08-21 17:44:42 -0700

    reactionCount use virtuals on thoughts model

--- models/thoughts.js
+++ models/thoughts.js
@@ -11,8 +11,16 @@ const thoughtSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now },
   username: { type: String, required: true },
   reactions: [reactionsSchema],
-});
+},
+{ // Tell the schema that it can use virtuals
+  toJSON: {
+    virtuals: true,
+  },
+  id: false,
+}
+);
 
+//create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query
 thoughtSchema.virtual('reactionCount').get(function () {
   return this.reactions.length;
 });
